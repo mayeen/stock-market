e1eb69513bfa8d43d80a40d4e28cf558f1203ef3
Shahriar <km.shahriar28@gmail.com>
2024-09-29 13:09:58 +0200

    get api implemented for stock

--- api/Program.cs
+++ api/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -24,5 +25,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.MapControllers();
 app.Run();
